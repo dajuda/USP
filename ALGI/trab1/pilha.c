17f8e24a9fb44991498863de8f75d05a44163033
Cainã D'Ajuda <caina@DajudaNotebook>
2015-09-20 19:00:02 -0300

    Created the necessary functions.

--- ALGI/trab1/pilha.c
+++ ALGI/trab1/pilha.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "pilha.h"
 
+
+//INICIA PILHA
+PILHA_DINAMICA *create_stack(){
+    PILHA_DINAMICA *aux;
+    aux = (PILHA_DINAMICA*) malloc (sizeof(PILHA_DINAMICA));
+    aux->tamanho = 0;
+    return (aux);
+}
 
 // CRIA UMA UNIDADE DA PILHA DINAMICA
 NO *unit(){
@@ -14,7 +23,7 @@ NO *unit(){
 void push(PILHA_DINAMICA *pilha, int x){
     NO *new;
     new = unit();
-    new->item = x;
+    *new->item = x;
     new->anterior = pilha->topo;
     pilha->topo = new;
     pilha->tamanho++;
@@ -25,10 +34,53 @@ void push(PILHA_DINAMICA *pilha, int x){
 int pop(PILHA_DINAMICA *pilha){
     int aux;
     NO *empty;
-    aux = pilha->topo->item;
+    aux = *pilha->topo->item;
     empty = pilha->topo;
     pilha->topo = pilha->topo->anterior;
     pilha->tamanho--;
     free(empty);
     return (aux);
+}
+
+void new_item(PILHA_DINAMICA *pilha, int x){
+    int aux, result;
+    aux = pop(pilha);
+    push(pilha, aux);
+    aux = (aux*10)+x;
+    push(pilha, aux);
+}
+
+void operation(PILHA_DINAMICA *pilha, int op){
+    int x, y;
+    if (op == soma){
+        y = pop(pilha);
+        x = pop(pilha);
+        push(pilha, x+y);
+    }
+    if (op == subtracao){
+        y = pop(pilha);
+        x = pop(pilha);
+        push(pilha, x-y);
+    }
+    if (op == divisao){
+        y = pop(pilha);
+        x = pop(pilha);
+        push(pilha, x/y);
+    }
+    if (op == multiplicacao){
+        y = pop(pilha);
+        x = pop(pilha);
+        push(pilha, x*y);
+    }
+}
+
+void insert_zero(PILHA_DINAMICA *pilha){
+    push(pilha, 0);
+}
+
+void empty_stack(PILHA_DINAMICA *pilha){
+    int aux;
+    while(pilha->tamanho != 0){
+        aux = pop(pilha);
+    }
 }
--- ALGI/trab1/pilha.h
+++ ALGI/trab1/pilha.h
@@ -11,6 +11,17 @@ struct pilha_dinamica{
     int tamanho;
 };
 
+enum OP{
+    soma= 1,
+    subtracao,
+    divisao,
+    multiplicacao
+};
+
+PILHA_DINAMICA *create_stack();
 NO *unit();
 void push(PILHA_DINAMICA *pilha, int x);
 int pop(PILHA_DINAMICA *pilha);
+void new_item(PILHA_DINAMICA *pilha, int x);
+void operation(PILHA_DINAMICA *pilha, int op);
+void empty_stack(PILHA_DINAMICA *pilha);
