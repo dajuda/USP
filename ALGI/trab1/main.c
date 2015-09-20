f57bb170ef55a23adfda350af267a7985f16f630
Benvides <vi.benevides@hotmail.com>
2015-09-20 16:45:21 -0300

    inicializacao do main.c

--- ALGI/trab1/main.c
+++ ALGI/trab1/main.c
@@ -1,7 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "pilha.h"
+
+#define debug 1
 
 int main(){
+
+/*-----Variaveis-----*/
+char *entrada;
+
+PILHA_DINAMICA *Pilha;
+
+int i;
+
+if(debug)
+	printf("Insira os valores: ");
+fegts("%s", &entrada);
+
+for(i=0;entrada[i]!='\0';i++){
+	//compara se a entrada é diferente de espaço
+	if(entrada[i]!=' '){
+		
+	}
+}
 
 return 0;
 }
